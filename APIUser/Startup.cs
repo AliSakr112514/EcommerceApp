1130d69d1b72af3d814b67201332674e81a1145b
aly511 <alyxmomdouh@gmail.com>
2022-01-02 22:31:00 +0200

    add

--- APIUser/Startup.cs
+++ APIUser/Startup.cs
@@ -91,7 +91,7 @@ namespace APIUser
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "APIUser", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "APIUser", Version = "v1" }); 
             });
 
            
@@ -102,9 +102,13 @@ namespace APIUser
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                 //app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "APIUser v1"));
+                app.UseSwaggerUI(c => 
+                { 
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "APIUser v1");
+                    c.RoutePrefix = string.Empty;
+                    });
             }
 
             app.UseHttpsRedirection();
